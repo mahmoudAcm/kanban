731c0a8c2070d7c151c6268573b9265c6b78c043
mahmoudAcm <mahmoud.acm.123@gmail.com>
2023-07-11 21:07:55 +0300

    Added main landmark and adjusted the second row of the grid to have a minimum height of 400px. Also, removed the div wrapping the header and transferred its styles to the appBar component.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -105,7 +105,16 @@ const BoardMenuButton = styled(IconButton)(({ theme }) => ({
 export default function Header() {
   const boardName = new Array(30).fill('Platform Launch').join(' ');
   return (
-    <AppBar position='sticky' elevation={0} color='transparent' sx={{ height: '100%' }}>
+    <AppBar
+      position='sticky'
+      elevation={0}
+      color='transparent'
+      sx={{
+        height: '100%',
+        background: theme => (theme.palette.__mode === 'DARK' ? 'var(--dark-grey)' : 'white'),
+        borderBottom: theme => `1px solid ${theme.palette.divider}`
+      }}
+    >
       <StyledToolbar>
         <Logo width='24' height='25' viewBox='0 0 24 25' fill='none' xmlns='http://www.w3.org/2000/svg'>
           <rect width='6' height='25' rx='2' fill='#635FC7' />
--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -6,16 +6,16 @@ import Sidebar from '@/src/components/Sidebar';
 const MainLayoutRoot = styled(Box)(({ theme }) => ({
   display: 'grid',
   gridTemplateColumns: '300px minmax(280px, 1fr)',
-  gridTemplateRows: '96px 1fr',
+  gridTemplateRows: '96px minmax(400px, 1fr)',
   minHeight: '100vh',
   transition: theme.transitions.create('grid-template-columns'),
   [theme.breakpoints.down('md')]: {
     gridTemplateColumns: '260px minmax(280px, 1fr)',
-    gridTemplateRows: '80px 1fr'
+    gridTemplateRows: '80px minmax(400px, 1fr)'
   },
   [theme.breakpoints.down('sm')]: {
     gridTemplateColumns: '1fr',
-    gridTemplateRows: '64px 1fr'
+    gridTemplateRows: '64px minmax(400px, 1fr)'
   }
 }));
 
@@ -23,15 +23,10 @@ export default function MainLayout({ children }: { children: ReactNode }) {
   return (
     <MainLayoutRoot>
       <Sidebar />
-      <Box
-        sx={{
-          background: theme => (theme.palette.__mode === 'DARK' ? 'var(--dark-grey)' : 'white'),
-          borderBottom: theme => `1px solid ${theme.palette.divider}`
-        }}
-      >
-        <Header />
+      <Header />
+      <Box component='main' sx={{ height: '100%', overflow: 'auto' }}>
+        {children}
       </Box>
-      <Box sx={{ height: '100%', overflow: 'auto' }}>{children}</Box>
     </MainLayoutRoot>
   );
 }
