eb70f1bd2708c4995e447c93c0f9e9840730d2d5
mahmoudAcm <mahmoud.acm.123@gmail.com>
2023-07-12 12:07:31 +0300

    Refactor: Utilize CSS variable for header height to increase reusability across different components.

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -4,18 +4,21 @@ import Header from '@/src/components/Header';
 import Sidebar from '@/src/components/Sidebar';
 
 const MainLayoutRoot = styled(Box)(({ theme }) => ({
+  '--header-height': '96px',
   display: 'grid',
   gridTemplateColumns: '300px minmax(280px, 1fr)',
-  gridTemplateRows: '96px minmax(400px, 1fr)',
+  gridTemplateRows: 'var(--header-height) minmax(400px, 1fr)',
   minHeight: '100vh',
   transition: theme.transitions.create('grid-template-columns'),
   [theme.breakpoints.down('md')]: {
+    '--header-height': '80px',
     gridTemplateColumns: '260px minmax(280px, 1fr)',
-    gridTemplateRows: '80px minmax(400px, 1fr)'
+    gridTemplateRows: 'var(--header-height) minmax(400px, 1fr)'
   },
   [theme.breakpoints.down('sm')]: {
+    '--header-height': '64px',
     gridTemplateColumns: '1fr',
-    gridTemplateRows: '64px minmax(400px, 1fr)'
+    gridTemplateRows: 'var(--header-height) minmax(400px, 1fr)'
   }
 }));
 
